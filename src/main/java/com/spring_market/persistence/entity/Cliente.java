0ba82675b8506d7b5478fe2370abb0886230ebba
itsluismario <itsluismario@gmail.com>
2025-01-23 16:41:28 -0600

    🗃️ Add entity classes for the Spring Market application
    - Added entity classes for the following tables:
      - `Categoria`: Represents the CATEGORIAS table.
      - `Cliente`: Represents the CLIENTES table.
      - `Compra`: Represents the COMPRAS table.
      - `ComprasProducto`: Represents the COMPRAS_PRODUCTOS table.
      - `ComprasProductoPK`: Represents the composite primary key for the COMPRAS_PRODUCTOS table.
      - `Producto`: Represents the PRODUCTOS table.
    
    - Relationships between entities have not been implemented yet. This commit focuses on the basic structure of the entities.
    
    These entities will serve as the foundation for the Spring Market application's data model.

---
+++ src/main/java/com/spring_market/persistence/entity/Cliente.java
@@ -0,0 +1,22 @@
+package com.spring_market.persistence.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private Long celular;
+
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+}
