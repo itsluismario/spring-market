b246b07755c94c09dda33b4ea9eff90ed5acea89
itsluismario <itsluismario@gmail.com>
2025-01-27 17:50:50 -0600

    ✨ Implement query methods in ProductoCrudRepository and integrate into ProductoRepository
    - Added custom query method `findByIdCategoriaOrderByNombreASC` to retrieve products by category ID ordered by name in ascending order.
    - Added custom query method `findByCantidadStockLessThanAndEstado` to fetch products with stock less than a specified amount and a specific state.
    - Integrated query methods into `ProductoRepository` with methods `getByCategoria` and `getEscasos` for better service layer access.

--- src/main/java/com/spring_market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/spring_market/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,11 @@ package com.spring_market.persistence.crud;
 import com.spring_market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreASC(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
