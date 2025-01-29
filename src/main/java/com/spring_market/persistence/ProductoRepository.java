71cd6b92738b07e280443e16873c271e5e6f5c29
itsluismario <itsluismario@gmail.com>
2025-01-29 10:49:42 -0600

    ✨ Autowired annotation for dependency injection in ProductoRepository
    Adds Spring's @Autowired annotation to inject ProductoCrudRepository and ProductMapper dependencies, enabling automatic dependency management in the repository layer.

--- src/main/java/com/spring_market/persistence/ProductoRepository.java
+++ src/main/java/com/spring_market/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.spring_market.domain.repository.ProductRepository;
 import com.spring_market.persistence.crud.ProductoCrudRepository;
 import com.spring_market.persistence.entity.Producto;
 import com.spring_market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,9 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository implements ProductRepository {
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+    @Autowired
     private ProductMapper mapper;
     @Override
     public List<Product> getAll() {
